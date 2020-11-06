3ddb8a44493fd95ef994335882887f3e5755831f
Pongpat Phokeed <fhluky@gmail.com>
2020-11-06 12:47:03 +0700

    port

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ docker stop br1
 docker rm br1
 docker rmi br1:latest
 docker build -f Dockerfile1 . -t br1
-docker run --name br1 --network pro-network -d -p 8080:8081 br1:latest
+docker run --name br1 --network pro-network -d -p 8081:8080 br1:latest
 
 docker stop br2
 docker rm br2
 docker rmi br2:latest
 docker build -f Dockerfile2 . -t br2
-docker run --name br2 --network pro-network -d -p 8080:8082 br2:latest
+docker run --name br2 --network pro-network -d -p 8082:8080 br2:latest
