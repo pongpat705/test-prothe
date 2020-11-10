ac6a817924ed3327ba46e45519bd5c0240c5f28d
Pongpat Phokeed <fhluky@gmail.com>
2020-11-10 11:07:41 +0700

    fix libs

--- br2/src/main/java/org/maoz/prothemeus/br2/Br2Application.java
+++ br2/src/main/java/org/maoz/prothemeus/br2/Br2Application.java
@@ -2,7 +2,6 @@ package org.maoz.prothemeus.br2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class Br2Application {
 
